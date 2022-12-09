5fc0b03c7fd8daf3e8859aba6c432d0c902f05de
fitmeat <eenot825@gmail.com>
2022-12-09 22:09:00 +0300

    Выполнил задание 43

---
+++ task43/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+double x = (b2-b1)/(k1-k2);
+double y = k1 * x + b1;
+Console.WriteLine($"Точка пересечения: ({x};{y})");
